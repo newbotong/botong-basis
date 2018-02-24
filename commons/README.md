fddabb15dc2c96f8d962f7eb45c36889c49e02d4
zhangxiao <zhangxiao@vv.cc>
2018-02-24 14:22:46 +0800

    init

---
+++ commons/README.md
@@ -0,0 +1,7 @@
+# 组件common说明
+该组件中，对服务的基础问题进行抽象设置，避免了每个服务上进行重复性设置，以及公共工具类的设置
+
+> <font size=6>抽象设置内容</font>
+
+服务启动类：BaseApplication
+服务控制类：BaseController
---
+++ commons/src/main/java/com/yunjing/mommon/constant/BaseCodeConstant.java
@@ -0,0 +1,21 @@
+package com.yunjing.mommon.constant;
+
+/**
+ * 响应码基类
+ * @version 1.0.0
+ * @author: zhangx
+ * @date 2018/1/20 11:10
+ * @description
+ **/
+public class BaseCodeConstant {
+
+    /** 成功响应码 */
+    public final static String SUCCESS = "000000";
+
+    /** 异常响应码 */
+    public final static String ERROR = "999999";
+
+    /** 参数错误响应码 */
+    public final static String PARAM_NULL = "000001";
+
+}
