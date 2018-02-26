b36cef71e0f57b052b07afbf6fd2474f791b5b13
zhangxiao <zhangxiao@vv.cc>
2018-02-26 14:54:37 +0800

    init

--- botong-eureka/src/main/java/com/yunjing/eureka/EurekaApplication.java
+++ botong-eureka/src/main/java/com/yunjing/eureka/EurekaApplication.java
@@ -2,24 +2,28 @@ package com.yunjing.eureka;
 
 import com.yunjing.mommon.base.BaseApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 /**
- * 祖册中心启动类
+ * 注册中心中心启动类
  * @version 1.0.0
  * @author: zhangx
  * @date 2018/2/24 15:31
  * @description
  **/
+@RestController
 @SpringBootApplication
 @EnableEurekaServer
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class EurekaApplication extends BaseApplication{
+
+    @GetMapping("/info")
+    public String info(){
+        return "info";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
--- botong-zuul/src/main/java/com/yunjing/zuul/ZuulApplication.java
+++ botong-zuul/src/main/java/com/yunjing/zuul/ZuulApplication.java
@@ -3,6 +3,7 @@ package com.yunjing.zuul;
 import com.yunjing.mommon.base.BaseApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @description
  **/
 @RestController
+@EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulApplication extends BaseApplication {
