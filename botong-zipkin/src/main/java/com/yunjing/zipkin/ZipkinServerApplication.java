dd15462c5b942d732a120ba378d32fe9f3153671
Gyb <guoyibin@vv.cc>
2018-02-27 16:59:54 +0800

    import zipkin server

---
+++ botong-zipkin/src/main/java/com/yunjing/zipkin/ZipkinServerApplication.java
@@ -0,0 +1,16 @@
+package com.yunjing.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+@EnableZipkinStreamServer
+@SpringBootApplication
+@EnableEurekaClient
+public class ZipkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+}
