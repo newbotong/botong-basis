b12bdbacc7bd53526aeb9a417cb6df9dbbe5c871
谈东魁 <tandongkui@vv.cc>
2018-03-21 19:16:27 +0800

    创建健康监控服务

---
+++ botong-admin/src/main/java/com/yunjing/config/AdminApplication.java
@@ -0,0 +1,23 @@
+package com.yunjing.config;
+
+import com.yunjing.mommon.base.BaseApplication;
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 健康监控服务启动类
+ * @author tandk
+ * @date 2018/3/21 17:41
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableAdminServer
+public class AdminApplication extends BaseApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
