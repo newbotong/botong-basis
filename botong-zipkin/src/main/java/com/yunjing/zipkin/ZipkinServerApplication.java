d410fcf7ee3ca36f28f1071b5ac072cb94415bc2
Gyb <guoyibin@vv.cc>
2018-04-12 10:57:56 +0800

    修改zipkin配置

--- botong-zipkin/src/main/java/com/yunjing/zipkin/ZipkinServerApplication.java
+++ botong-zipkin/src/main/java/com/yunjing/zipkin/ZipkinServerApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import zipkin.storage.mysql.MySQLStorage;
+
+import javax.sql.DataSource;
+
 
 /**
  * @version 1.0.0
@@ -17,5 +23,12 @@ public class ZipkinServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+
+
+    @Bean
+    @Primary
+    public MySQLStorage mySQLStorage(DataSource dataSource) {
+        return MySQLStorage.builder().datasource(dataSource).executor(Runnable::run).build();
     }
 }
