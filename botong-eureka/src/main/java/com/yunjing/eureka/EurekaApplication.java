3e789ff468f39d432db6cfd8ea7a985b233c9f04
Gyb <guoyibin@vv.cc>
2018-04-21 15:55:14 +0800

    升级commons-2.0.0.6

--- botong-eureka/src/main/java/com/yunjing/eureka/EurekaApplication.java
+++ botong-eureka/src/main/java/com/yunjing/eureka/EurekaApplication.java
@@ -1,6 +1,5 @@
 package com.yunjing.eureka;
 
-import com.yunjing.mommon.base.BaseApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 注册中心中心启动类
+ *
  * @version 1.0.0
  * @author: zhangx
  * @date 2018/2/24 15:31
@@ -18,12 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication extends BaseApplication{
-
-    @GetMapping("/info")
-    public String info(){
-        return "info";
-    }
+public class EurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
